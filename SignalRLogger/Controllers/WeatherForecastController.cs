07ea5102b7d1aef7f5f5a1d8f54738fcda41edfd
iwaitu <iwaitu@aliyun.com>
2022-07-28 17:23:01 +0800

    ...

--- SignalRLogger/Controllers/WeatherForecastController.cs
+++ SignalRLogger/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using System.Diagnostics;
 
 namespace SignalRLoggerDemo.Controllers
 {
@@ -36,8 +37,44 @@ namespace SignalRLoggerDemo.Controllers
             _logger.LogInformation("return " + ret.Length + " results.");
                         
             return ret;
+        }
 
+        /// <summary>
+        /// https://localhost:7244/WeatherForecast/readlogs?id=3974d1f2899e
+        /// </summary>
+        /// <param name="id">容器id</param>
+        /// <returns></returns>
+        [HttpGet("readlogs")]
+        public async Task<string> ReadDockerLogs(string id)
+        {
+            using Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
 
+            cmd.StandardInput.WriteLine($"docker logs {id} --tail=10");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            
+            //string output = reader.ReadToEnd();
+            int i = 0;
+            
+            using StreamReader reader = cmd.StandardOutput;
+            
+            while (!reader.EndOfStream)
+            {
+                if (i > 3)
+                {
+                    _logger.LogInformation(await reader.ReadLineAsync());
+                }
+                i++;
+                await Task.Delay(10);
+            }
+            await cmd.WaitForExitAsync();
+            return "ok";
         }
     }
 }
